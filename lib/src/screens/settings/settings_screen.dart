0452e7ba15ea9b7f139ab12c19bbbccd56b03037
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-02-05 19:45:00 -0700

    fixed bug with user preferences bloc

--- lib/src/screens/settings/settings_screen.dart
+++ lib/src/screens/settings/settings_screen.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:grateful/src/blocs/user_preference/user_preference_bloc.dart';
 import 'package:grateful/src/screens/settings/notification_settings/notification_settings_widget.dart';
 import 'package:grateful/src/widgets/background_gradient_provider.dart';
 
 class SettingsScreen extends StatelessWidget {
   build(context) {
+    final UserPreferenceBloc userPreferenceBloc =
+        BlocProvider.of<UserPreferenceBloc>(context);
+    if (userPreferenceBloc is! UserPreferencesFetched) {
+      userPreferenceBloc.add(FetchUserPreferences());
+    }
     return Scaffold(
       appBar: AppBar(
         elevation: 0,

e508cf7f4819530660635ef2d4d7724162824382
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-03-08 22:00:00 -0600

    added animations to bottom navigation bar

--- lib/src/widgets/navigation_bar.dart
+++ lib/src/widgets/navigation_bar.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 typedef OnSelectTab = void Function(int tabIndex);
 
-class NavigationBar extends StatelessWidget {
+class NavigationBar extends StatefulWidget {
   const NavigationBar(
       {@required this.onSelectTab, @required this.currentIndex});
 
@@ -11,29 +11,45 @@ class NavigationBar extends StatelessWidget {
   final int currentIndex;
 
   @override
+  _NavigationBarState createState() => _NavigationBarState();
+}
+
+class _NavigationBarState extends State<NavigationBar> {
+  @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
+    final List<IconData> pageIcons = <IconData>[
+      Icons.edit,
+      Icons.list,
+      Icons.settings
+    ];
     return BottomNavigationBar(
-      currentIndex: currentIndex,
-      onTap: onSelectTab,
-      elevation: 30,
-      backgroundColor: theme.colorScheme.primaryVariant,
-      selectedIconTheme:
-          theme.primaryIconTheme.copyWith(color: theme.colorScheme.onSecondary),
-      unselectedIconTheme: theme.primaryIconTheme
-          .copyWith(color: theme.colorScheme.onSecondary.withOpacity(0.4)),
-      unselectedLabelStyle: const TextStyle(color: Colors.white12),
-      items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-            icon: Icon(Icons.edit),
-            title: Text('Journal', style: theme.primaryTextTheme.body1)),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.list),
-            title: Text('Feed', style: theme.primaryTextTheme.body1)),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            title: Text('Settings', style: theme.primaryTextTheme.body1))
-      ],
-    );
+        currentIndex: widget.currentIndex,
+        onTap: widget.onSelectTab,
+        elevation: 30,
+        backgroundColor: theme.colorScheme.primaryVariant,
+        selectedIconTheme: theme.primaryIconTheme
+            .copyWith(color: theme.colorScheme.onSecondary),
+        unselectedIconTheme: theme.primaryIconTheme
+            .copyWith(color: theme.colorScheme.onSecondary.withOpacity(0.4)),
+        unselectedLabelStyle: const TextStyle(color: Colors.white12),
+        items: pageIcons.map((IconData icon) {
+          return BottomNavigationBarItem(
+            icon: navigationBarItem(pageIcons.indexOf(icon), icon),
+            title: Container(),
+          );
+        }).toList());
+  }
+
+  Widget navigationBarItem(int index, IconData icon) {
+    return AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        curve: Curves.easeIn,
+        width: widget.currentIndex == index ? 30 : 20,
+        height: widget.currentIndex == index ? 30 : 20,
+        child: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+          return Icon(icon, size: constraints.biggest.height);
+        }));
   }
 }
